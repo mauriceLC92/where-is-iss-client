426bc413ea3e827fae3b21d4262b3dca28fa8f9a
Maurice Le Cordier <mauricelecordier@gmail.com>
2021-06-27 20:37:05 +0200

    removed unused css styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Where is the ISS</title>
+                <meta name="description" content="Track the ISS" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
-export default MyApp
+export default MyApp;
