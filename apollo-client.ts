390600cd24cc90a64b5c5b41d739aaef74e51cfd
Maurice Le Cordier <mauricelecordier@gmail.com>
2021-06-29 01:22:03 +0200

    adding real url

--- apollo-client.ts
+++ apollo-client.ts
@@ -2,9 +2,12 @@ import { ApolloClient, gql, NormalizedCacheObject } from '@apollo/client';
 
 import { cache } from './cache';
 
+const isProd = process.env.NODE_ENV === 'production';
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     cache,
-    uri: 'http://localhost:4000/graphql',
+    uri: isProd
+        ? process.env.NEXT_PUBLIC_PROD_URL
+        : 'http://localhost:4000/graphql',
 });
 
 export default client;
